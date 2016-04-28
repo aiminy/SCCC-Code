fe01f0b1adfa555008ff57aecbac2e4c30757d20
aiminy <jlfmssm@gmail.com>
2016-04-28 15:43:41 -0400

    Bash code from media DATA

---
+++ Input4Heatmap2.sh
@@ -0,0 +1,39 @@
+#case1
+cp H3K4me3.png heatmap/case1
+cp H3K27me3.png heatmap/case1
+cp H3K4me3--H3K27me3.png heatmap/case1
+cp H3K27me3--H3K4me3.png heatmap/case1
+
+#case2
+cp H2AK119ub1.png heatmap/case2
+cp H3K27me3.png heatmap/case2
+cp H3K27me3--H2AK119ub1.png heatmap/case2
+cp H2AK119ub1--H3K27me3.png heatmap/case2
+
+#case3
+cp Suz12.png heatmap/case3
+cp Ring1b-Suz12.png heatmap/case3
+cp Cbx7--Suz12.png heatmap/case3
+cp Rybp--Suz12.png heatmap/case3
+
+#case4
+cp Rybp.png heatmap/case4
+cp Suz12.png heatmap/case4
+cp Rybp--Suz12.png heatmap/case4
+
+#case5
+cp Ring1B.png heatmap/case5
+cp Suz12.png heatmap/case5
+cp Ring1b-Suz12.png heatmap/case5
+cp Suz12--Ring1b.png heatmap/case5
+
+#case6
+cp Ring1B.png heatmap/case6
+cp Cbx7.png heatmap/case6
+cp Rybp.png heatmap/case6
+cp Suz12.png heatmap/case6
+cp Rybp--Suz12.png heatmap/case6 
+
+#case7
+cp Rybp.png heatmap/case7
+cp Rybp--Suz12.png heatmap/case7
---
+++ Re_config_All_27_Config_Heatmap_files.sh
@@ -0,0 +1,40 @@
+qqq#Case1
+cat Config_Heatmap_1.txt > Order_first_Chip_case1-12.txt 
+cat Config_Heatmap_2.txt >> Order_first_Chip_case1-12.txt 
+cat Config_Heatmap_3.txt > Order_first_Chip_case1-3.txt
+cat Config_Heatmap_4.txt > Order_first_Chip_case1-4.txt
+
+#Case2
+cat Config_Heatmap_5.txt > Order_first_Chip_case2-56.txt
+cat Config_Heatmap_6.txt >> Order_first_Chip_case2-56.txt
+cat Config_Heatmap_7.txt > Order_first_Chip_case2-7.txt
+cat Config_Heatmap_8.txt > Order_first_Chip_case2-8.txt
+
+#Case3
+cat Config_Heatmap_9.txt > Order_first_Chip_case3-9.txt
+cat Config_Heatmap_10.txt > Order_first_Chip_case3-10.txt
+cat Config_Heatmap_11.txt > Order_first_Chip_case3-11.txt
+cat Config_Heatmap_12.txt > Order_first_Chip_case3-12.txt
+
+#Case4
+cat Config_Heatmap_13.txt > Order_first_Chip_case4-13-14.txt
+cat Config_Heatmap_14.txt >> Order_first_Chip_case4-13-14.txt
+cat Config_Heatmap_15.txt > Order_first_Chip_case4-15.txt
+
+#Case5
+cat Config_Heatmap_16.txt > Order_first_Chip_case5-16-17.txt
+cat Config_Heatmap_17.txt >> Order_first_Chip_case5-16-17.txt
+cat Config_Heatmap_18.txt > Order_first_Chip_case5-18.txt
+cat Config_Heatmap_19.txt > Order_first_Chip_case5-19.txt 
+
+#Case6
+cat Config_Heatmap_20.txt > Order_first_Chip_case6-20-21-22-23.txt
+cat Config_Heatmap_21.txt >> Order_first_Chip_case6-20-21-22-23.txt
+cat Config_Heatmap_22.txt >> Order_first_Chip_case6-20-21-22-23.txt
+cat Config_Heatmap_23.txt >> Order_first_Chip_case6-20-21-22-23.txt
+cat Config_Heatmap_24.txt > Order_first_Chip_case6-24.txt
+
+#Case7
+cat Config_Heatmap_25.txt > Order_first_Chip_case7-25.txt 
+cat Config_Heatmap_26.txt > Order_first_Chip_case7-26.txt
+cat Config_Heatmap_27.txt > Order_first_Chip_case7-27.txt
